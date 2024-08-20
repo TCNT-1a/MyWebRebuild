71fe08b690d7edb485246445f7bbfa7efbb3b866
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-08-20 17:39:01 +0700

    up

---
+++ MyApp.Web/Controllers/SystemInforController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Web.Controllers
+{
+    public class SystemInforController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
