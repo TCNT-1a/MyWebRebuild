406e34f24cf8aff34b99492a13cc8262e208b547
Ta Chi Nguyen <tcnguyen93@gmail.com>
2024-08-21 17:24:24 +0700

    up

---
+++ MyApp.Web/Models/PostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyApp.Web.Abstraction;
+using MyApp.Web.Infra.Data;
+
+namespace MyApp.Web.Models
+{
+    public class PostViewModel: BlogEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int PostView { get; set; }
+        public HeadingTag HeadingTag { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
